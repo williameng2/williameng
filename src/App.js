7d0a4294d76d1b543292ddd153b9160a9bb9c9ea
William Eng <1williameng@gmail.com>
2023-01-03 23:04:26 -0800

    logo and header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from "./logo.png"
+import resume from "./Eng_William.pdf"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
+
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div className='navbar'>
+        <div><img src={logo} className="logo" alt='Eng-William-Logo' ></img></div>
+        <button type="button">Work</button>
+        <button type='button'>About</button>
+        <a href={resume} target="_blank">Resume</a>
+      </div>
+    );
+  }
+};
+
 
 export default App;
